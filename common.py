c7ce7b76496158759059144db9ffc916fae36c99
Fatih Eser <afeserr@gmail.com>
2022-03-06 13:58:49 +0300

    new encryption, client

--- common.py
+++ common.py
@@ -7,19 +7,18 @@ tag3 = b'a'
 data_received = 0
 
 def encrypt(data):
-    return b''.join([b'a' + data[index:index+1] for index in range(len(data))])
+    return b''.join([tag1 + data[index:index+1] for index in range(len(data))])
 
 def decrypt(data):
     global data_received
 
     output_data = b''
     for index in range(len(data)):
-        data_received = data_received + 1
-
         # skip tag 3 => b'a'
-        if data_received % 2 == 0:
+        if data_received % (len(tag1) + 1) == len(tag1):
             output_data = output_data + data[index:index+1]
 
+        data_received = data_received + 1
     return output_data
 
 def partial_send(conn, data):
